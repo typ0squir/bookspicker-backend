1b534123e21a27aec1c15232add608fd2ca5fc08
typ0squir <typ0squir@gmail.com>
2025-12-23 13:51:47 +0900

    환경 변수 수정

--- bookspicker/settings.py
+++ bookspicker/settings.py
@@ -1,4 +1,5 @@
 import environ
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -7,17 +8,6 @@ env = environ.Env()
 environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-import environ
-import os
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-# Reading .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
