5224ffe28fc81ac56d2b0d310fbf2c1f3374e310
Ubuntu <ubuntu@ip-172-31-42-212.ap-northeast-2.compute.internal>
2025-12-23 07:48:07 +0000

    Sync from server

--- bookspicker/settings.py
+++ bookspicker/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
